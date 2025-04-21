d2096d73d3fce077d48d842ff351edff11c88325
Abdullah Bin Mamun <binbean44@gmail.com>
2025-04-21 17:00:55 +0600

    Add StatusService Interface and class

---
+++ src/TaskFlow/TaskFlow.Application/StatusService.cs
@@ -0,0 +1,17 @@
+﻿using TaskFlow.Domain;
+using TaskFlow.Domain.Entities;
+using TaskFlow.Domain.ServiceInterfaces;
+
+namespace TaskFlow.Application
+{
+    public class StatusService(ITaskFlowUnitOfWork taskFlowUnitOfWork) : IStatusService
+    {
+        private readonly ITaskFlowUnitOfWork _taskFlowUnitOfWork = taskFlowUnitOfWork;
+
+
+        public async Task<(IList<Status> data, int total, int totalDisplay)> GetAllStatusAsync()
+        {
+            return await _taskFlowUnitOfWork.StatusRepository.GetDynamicStatusAsync();
+        }
+    }
+}

83ed56bd099303771b7fde9cebfce798f68525eb
Abdullah Bin Mamun <binbean44@gmail.com>
2025-04-21 11:36:58 +0600

    Create and set up initial project

---
+++ src/TaskFlow/TaskFlow.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskFlow.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}

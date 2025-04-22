a5dcb468404667ca921f106a1f7f203fa801dd34
Abdullah Bin Mamun <binbean44@gmail.com>
2025-04-22 16:23:24 +0600

    Create ToSelectList Extension to Create List to SelectListItem

---
+++ src/TaskFlow/TaskFlow.Web/Models/TaskCreateModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using TaskFlow.Domain.Entities;
+using TaskFlow.Infrastructure;
+
+namespace TaskFlow.Web.Models
+{
+    public class TaskCreateModel
+    {
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public Guid? StatusId { get; set; }
+        public Priority Priority { get; set; }
+        public List<Guid>? PrerequisiteIds { get; set; }
+        public IList<SelectListItem>? AllTasks { get; private set; }
+
+        public void GetAllTasks(IList<TaskItem> taskItems)
+        {
+            AllTasks = taskItems.ToSelectList(x => x.Title, y => y.Id.ToString());
+        }
+    }
+}

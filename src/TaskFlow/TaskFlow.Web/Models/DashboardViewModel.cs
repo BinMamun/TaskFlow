a776c4e548d330268d9a657e4dd2882364b382af
Abdullah Bin Mamun <binbean44@gmail.com>
2025-04-25 11:50:25 +0600

    Create DashboardViewModel.cs

---
+++ src/TaskFlow/TaskFlow.Web/Models/DashboardViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskFlow.Web.Models
+{
+    public class DashboardViewModel
+    {
+        public int Pending { get; set; }
+        public int InProgress { get; set; }
+        public int Completed { get; set; }
+    }
+}

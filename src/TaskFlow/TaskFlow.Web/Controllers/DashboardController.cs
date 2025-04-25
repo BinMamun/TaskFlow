77af8634d8f481b4405460159b13ed2bc26ce618
Abdullah Bin Mamun <binbean44@gmail.com>
2025-04-25 11:49:41 +0600

    Create DashboardController.cs

---
+++ src/TaskFlow/TaskFlow.Web/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskFlow.Web.Controllers
+{
+    public class DashboardController(ILogger<DashboardController> logger) : Controller
+    {
+        private readonly ILogger<DashboardController> _logger = logger;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

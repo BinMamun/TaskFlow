8305d11ad046ee7f20eee830ae03ec8408eab874
Abdullah Bin Mamun <binbean44@gmail.com>
2025-04-25 12:17:47 +0600

    fetch dashboard Index model data

--- src/TaskFlow/TaskFlow.Web/Controllers/DashboardController.cs
+++ src/TaskFlow/TaskFlow.Web/Controllers/DashboardController.cs
@@ -1,14 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TaskFlow.Domain.ServiceInterfaces;
+using TaskFlow.Web.Models;
 
 namespace TaskFlow.Web.Controllers
 {
-    public class DashboardController(ILogger<DashboardController> logger) : Controller
+    public class DashboardController(
+        ILogger<DashboardController> logger,
+        ITaskItemService taskItemService
+        ) : Controller
     {
         private readonly ILogger<DashboardController> _logger = logger;
+        private readonly ITaskItemService _taskItemService = taskItemService;
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var model = new DashboardViewModel();
+            model.Pending = await _taskItemService.PendingTaskCountAsync();
+            model.InProgress = await _taskItemService.InProgressTaskCountAsync();
+            model.Completed = await _taskItemService.CompletedTaskCountAsync();
+            return View(model);
+        }
+
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }

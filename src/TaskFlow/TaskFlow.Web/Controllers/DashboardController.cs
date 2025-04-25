22138678254ea124abcd75ea0c21ba77f26f0b5a
Abdullah Bin Mamun <binbean44@gmail.com>
2025-04-25 14:05:04 +0600

    Adjust some frontend design

--- src/TaskFlow/TaskFlow.Web/Controllers/DashboardController.cs
+++ src/TaskFlow/TaskFlow.Web/Controllers/DashboardController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Diagnostics;
+using System.Web;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TaskFlow.Domain.ServiceInterfaces;
 using TaskFlow.Web.Models;
 
 namespace TaskFlow.Web.Controllers
 {
+    [Authorize]
     public class DashboardController(
         ILogger<DashboardController> logger,
         ITaskItemService taskItemService
@@ -15,14 +18,45 @@ namespace TaskFlow.Web.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var model = new DashboardViewModel();
-            model.Pending = await _taskItemService.PendingTaskCountAsync();
-            model.InProgress = await _taskItemService.InProgressTaskCountAsync();
-            model.Completed = await _taskItemService.CompletedTaskCountAsync();
+            var model = new DashboardViewModel
+            {
+                Pending = await _taskItemService.PendingTaskCountAsync(),
+                InProgress = await _taskItemService.InProgressTaskCountAsync(),
+                Completed = await _taskItemService.CompletedTaskCountAsync()
+            };
             return View(model);
         }
 
-        
+        [HttpPost]
+        public async Task<JsonResult> GetUpcomingDeadLineTasksJsonData([FromBody] TaskListModel model)
+        {
+            var result = await _taskItemService.GetAllTasksAsync(
+                model.PageIndex,
+                model.PageSize,
+                model.SearchParams,
+                model.FormatSortExpression("DueDate", "Title", "Description", "Priority", "StatusId", "StatusName"));
+
+            var data = new
+            {
+                recordsTotal = result.total,
+                recordsFiltered = result.totalDisplay,
+                data = (from records in result.data
+                        select new string[]
+                            {
+                                HttpUtility.HtmlEncode(records.DueDate.ToString("dd-MMM-yyyy")),
+                                HttpUtility.HtmlEncode(records.Title),
+                                HttpUtility.HtmlEncode(records.Description),
+                                HttpUtility.HtmlEncode(records.Priority),
+                                HttpUtility.HtmlEncode(records.Status.StatusName.ToString()),
+                                records.Id.ToString()
+                            }
+                        ).ToArray()
+            };
+            return Json(data);
+        }
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
